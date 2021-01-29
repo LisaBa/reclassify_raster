ce20f92b3d3aae4d78b8675f8d80484042878973
LisaBa <lisa.bareiss@posteo.de>
2021-01-29 16:34:26 +0100

    Edit

--- test.py
+++ test.py
@@ -2,18 +2,18 @@ from reclassify_raster import RasterReclassification as RR
 import matplotlib.pyplot as plt
 import numpy
 import rasterio as rio
+import os
 
 files = (
     "C:/Users/Lisapisa/Documents/Master/Masterthesis/01-Raster/samples_negative/labels"
 )
 exp = "C:/Users/Lisapisa/Documents/Master/Masterthesis/01-Raster/samples_negative_edited/labels"
 
-new = RR(files, exp)
-new.reclassify(new_value=0)
+# new = RR(files, exp)
+# new.reclassify(new_value=0)
 
 # Check created TIF file
-# src = rio.open("example.tif")
-# img = src.read()
-# src.close()
-# plt.imshow(img.squeeze())
-# plt.show()
+with rio.open(os.path.join(exp, "000000591.tif")) as src:
+    img = src.read()
+plt.imshow(img.squeeze())
+plt.show()
